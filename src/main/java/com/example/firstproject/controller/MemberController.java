c8cb4fde470e45d4ff6e7fecb84eaced38b98413
곽동욱 <kdw381@naver.com>
2025-07-12 16:05:57 +0900

    mustache

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -1,4 +1,10 @@
 package com.example.firstproject.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class MemberController {
+    @Autowired
+    private
 }
