7fc7cf513adf2e9395a4cd37631a6970708c7f76
곽동욱 <kdw381@naver.com>
2025-07-12 21:10:21 +0900

    롬복 이용한 리팩터링

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,25 +1,15 @@
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@ToString
+@AllArgsConstructor
 public class ArticleForm {
     // 아래 두개는 클라이언트로부터 전달받을 데이터
     private String title; // 제목을 받을 필드
     private String content; // 내용을 받을 필드
-
-    public ArticleForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    // 데이터를 잘 받았는지 확인할 toString() 메서드 추가
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     //DTO를 Entity로 바꿔주는 메서드 (DB저장 직전에 사용됨)
     public Article toEntity() {
