3f89ad88cdaddffc2d102e7985bb820ca169be8f
곽동욱 <kdw381@naver.com>
2025-07-15 01:08:05 +0900

    feat:전체조회 구현

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -29,10 +31,12 @@ public class ArticleController {
     public String createArticle(ArticleForm form){ // 폼 데이터를 DTO로 받기
         log.info(form.toString()); // println문 대신 로깅코드
         //System.out.println(form.toString()); // DTO에 폼 데이터가 잘 담겼는지?
+
         // 1. DTO를 엔티티로 변환
         Article article = form.toEntity(); // 폼데이터를 DB에 넣기위해 Entity로 변환
         log.info(article.toString()); // println문 대신 로깅코드
        // System.out.println(article.toString());
+
         // 2. 리파지터리로 엔티티를 DB에 저장
         Article saved = articleRepository.save(article);
         log.info(saved.toString()); // println문 대신 로깅코드
@@ -40,16 +44,32 @@ public class ArticleController {
         return "";
     }
 
+    // id를 통한 Read
     @GetMapping("/articles/{id}")
     public String show(@PathVariable Long id, Model model){ // 매개변수로 id 받아오기
         log.info("id = "+id);
         // 1. id를 조회해 데이터 가져오기
         Article articleEntity = articleRepository.findById(id).orElse(null);
+
         //   2. 모델에 데이터 등록
         model.addAttribute("article", articleEntity); // article이라는 이름으로 articleEntity 등록
+
         //   3. 뷰페이지 만들어 반환
         return "articles/show";
 
+    }
+
+    // List 전체조회
+    @GetMapping("/articles")
+    public String index(Model model){
+        // 1. DB에서 모든 Article 엔티티 데이터 가져오기
+        ArrayList<Article> articleEntityList = articleRepository.findAll(); // List<article>을 사용하면 DB에서 조회한 데이터 묶음을 리스트에 담긴 Article 타입으로 가져옴
+
+        // 2. 가져온 Article 묶음을 모델에 등록하기
+        model.addAttribute("articleList", articleEntityList); // 전달할 데이터 묶음인 article EntityList를 articleList라는 이름으로 등록함
+
+        // 3. 사용자에게 보여줄 뷰 페이지 설정하기
+        return "articles/index";
     }
 
 }
--- src/main/java/com/example/firstproject/repository/ArticleRepository.java
+++ src/main/java/com/example/firstproject/repository/ArticleRepository.java
@@ -3,7 +3,11 @@ package com.example.firstproject.repository;
 import com.example.firstproject.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 // 이걸 함으로써 ArticleRepository는 CRUD를 사용가능
 // 엔티티가 DB 속 테이블에 저장 및 관리될수 있게 함
 public interface ArticleRepository extends CrudRepository<Article,Long> { // Article : 관리대상 엔티티의 클래스타입, Long 관리대상 엔티티의 대푯값 타입
+    @Override
+    ArrayList<Article> findAll(); // Iterable -> ArrayList 수정때문에 오버라이딩 해줌.
 }
