6103ca6ae7599faf8bbe7304178202ed599b168f
곽동욱 <kdw381@naver.com>
2025-07-20 00:50:00 +0900

    feat:Delete

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -72,4 +72,49 @@ public class ArticleController {
         return "articles/index";
     }
 
+    // 수정
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id, Model model){
+        // 1. 수정할 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+
+        return "articles/edit";
+    }
+
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form){ // 매개변수로 DTO 받아오기
+        log.info(form.toString());
+        // 1. DTO를 엔티티로 변환
+        Article articleEntity = form.toEntity(); // DTO(form)를 엔티티(articleEntity)로 변환
+        log.info(articleEntity.toString());
+        // 2. 엔티티를 DB에 저장
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null); // DB에서 기존데이터 가져오기
+        if(target != null){
+            articleRepository.save(articleEntity); // 엔티티를 DB에 저장(갱신)
+        }
+        // 3. 수정 결과 페이지로 리다이렉트
+        return "redirect:/articles/" + articleEntity.getId();
+    }
+
+    @GetMapping("articles/{id}/delete")
+    public String delete(@PathVariable Long id){
+        log.info("삭제 요청이 들어왔습니다!!");
+
+        // 1. 삭제할 대상 가져오기
+        Article target = articleRepository.findById(id).orElse(null);
+        log.info(target.toString());
+
+        // 2. 대상 엔티티 삭제하기
+        if(target != null){
+            articleRepository.delete(target); // repository가 DB에서 target을 삭제
+        }
+
+        // 3. 결과 페이지로 리다이렉트
+        return "redirect:/articles";
+
+    }
+
 }
--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -7,12 +7,14 @@ import lombok.ToString;
 @ToString
 @AllArgsConstructor
 public class ArticleForm {
+    private Long id;
     // 아래 두개는 클라이언트로부터 전달받을 데이터
     private String title; // 제목을 받을 필드
     private String content; // 내용을 받을 필드
 
+
     //DTO를 Entity로 바꿔주는 메서드 (DB저장 직전에 사용됨)
     public Article toEntity() {
-        return new Article(null,title,content);
+        return new Article(id,title,content);
     }
 }
