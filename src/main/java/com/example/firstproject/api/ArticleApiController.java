38205199f5ab894cdf353dd6de22314fad1776b3
곽동욱 <kdw381@naver.com>
2025-07-29 22:25:53 +0900

    transaction

--- src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -2,7 +2,6 @@ package com.example.firstproject.api;
 
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
-import com.example.firstproject.repository.ArticleRepository;
 import com.example.firstproject.service.ArticleService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,47 +14,56 @@ import java.util.List;
 @Slf4j
 @RestController
 public class ArticleApiController {
-    @Autowired // 게시글 Repo 주입 (의존성 주입)
-    private ArticleService articleService; // 서비스 객체 주입
-    //GET
-    // 전체 게시글 조회
+    @Autowired
+    private ArticleService articleService;
+
+    // GET
     @GetMapping("/api/articles")
-    public List<Article> index() { // Article을 반환하므로 반환형이 List<Article>
+    public List<Article> index() {
         return articleService.index();
     }
-    // 단일 게시글 조회
+
     @GetMapping("/api/articles/{id}")
-    public Article show(@PathVariable Long id){
+    public Article show(@PathVariable Long id) {
         return articleService.show(id);
     }
 
-    //POST
+    // POST
     @PostMapping("/api/articles")
-    public ResponseEntity<Article> create(@RequestBody ArticleForm dto){
+    public ResponseEntity<Article> create(@RequestBody ArticleForm dto) {
         Article created = articleService.create(dto);
-        return (created != null)?
+        return (created != null) ?
                 ResponseEntity.status(HttpStatus.OK).body(created) :
                 ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+
     }
 
-//    //PATCH
+    // PATCH
     @PatchMapping("/api/articles/{id}")
-    public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm dto){
-    Article updated = articleService.update(id, dto);
-    return (updated != null)?
-            ResponseEntity.status(HttpStatus.OK).body(updated):
-            ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-    }
-
-    //DELETE
-    @DeleteMapping("/api/articles/{id}")
-    public ResponseEntity<Article> delete(@PathVariable Long id){
-        Article deleted = articleService.delete(id);
-        return (deleted != null) ?
-                ResponseEntity.status(HttpStatus.NO_CONTENT).build():
+    public ResponseEntity<Article> update(@PathVariable Long id,
+                                          @RequestBody ArticleForm dto) {
+        Article updated = articleService.update(id, dto);
+        return (updated != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(updated):
                 ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
+    // DELETE
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Article> delete(@PathVariable Long id) {
+        Article deleted = articleService.delete(id);
+        return (deleted != null) ?
+                ResponseEntity.status(HttpStatus.NO_CONTENT).build() :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
 
+    @PostMapping("/api/transaction-test")
+    public ResponseEntity<List<Article>> transactionTest(@RequestBody
+                                                         List<ArticleForm> dtos) {
+        List<Article> createdList = articleService.createArticles(dtos);
+        return (createdList != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(createdList) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
 
 }
